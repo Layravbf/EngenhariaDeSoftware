df23a5f0f92c7453aeb7b04c2d4ac5f09447cf4d
jh-ennifer <jhenniferrodrigues200119@gmail.com>
2022-04-19 19:30:21 -0300

    Ajustes no código de alteração dos dados

--- Projeto/controller/C_alterarMeusDados.php
+++ Projeto/controller/C_alterarMeusDados.php
@@ -1,4 +1,5 @@
 <?php
+    //Código feito para alterar os dados do usuário
     include_once("../persistence/connection.php");
     include_once("../persistence/usuarioDAO.php");
 
@@ -7,7 +8,7 @@
 
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-
+    // Verifica se o tamanho da senha é menor que 8, se for, é feita a alteração dos dados
     if(strlen($senha) < 8){
         $_SESSION['erroSenha'] = true;
         header('Location: http://localhost/Projeto/view/meusDados.php');
